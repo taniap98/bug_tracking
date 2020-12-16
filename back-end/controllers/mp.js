889e714b6a69afc0e83bd33211922cab1818d145
madalinap09 <madalinaf.pescaru@gmail.com>
2020-12-16 21:55:53 +0200

    Basic mp routes

--- back-end/controllers/mp.js
+++ back-end/controllers/mp.js
@@ -5,7 +5,74 @@ const router = express.Router();
 
 
 const controller = {
-    
+    addMp: async(req, res) => {
+        const mp = {
+           
+            projectId: req.body.projectId,
+            userId: req.body.userId
+        }
+
+        try{
+            const newMp = await MpDB.create(mp);
+            res.status(200).send({
+                message: "Mp added."
+            })
+        } catch(error){
+            console.log(error);
+            res.status(500).send({
+                message: "Error creating new mp!"
+            })
+        }
+    },
+
+    getAllMps: async(req, res) => {
+        try{
+            const mps = await MpDB.findAll();
+            res.status(200).send(mps);        
+        } catch(error){
+        console.log(error);
+        res.status(500).send({
+            message: "Error selecting all mps!"
+        })
+    }
+},
+
+    getOneMp: async(req, res) => {
+        try{
+        let mpId = req.path.split('/')[2];
+        const mp = await MpDB.findOne({
+            where: {
+                id: mpId
+            }
+        })
+        res.status(200).send(mp);
+    } catch(error){
+        console.log(error);
+        res.status(500).send({
+            message: "Error selecting one mp!"
+        })
+    }
+},
+
+    deleteOneMp: async(req, res) => {
+        try{
+            let mpId = req.path.split('/')[2];
+            const mp = await MpDB.destroy({
+                where: {
+                    id: mpId
+                }
+            })
+            res.status(200).send({
+                message: "Mp" + mpId + "deleted."
+            });
+        } catch(error){
+            console.log(error);
+            res.status(500).send({
+                message: "Error deleting user!"
+            })
+        }
+    }
+
 }
 
 module.exports = controller;
--- back-end/routes/mp.js
+++ back-end/routes/mp.js
@@ -2,6 +2,11 @@ const express = require("express");
 const router = express.Router();
 const mpController = require("../controllers/mp")
 
+router.post('/addmp', mpController.addMp);
+router.get('/mps', mpController.getAllMps);
+router.get('/mp/:id', mpController.getOneMp);
+router.delete('/deletemp/:id', mpController.deleteOneMp);
+
 
 
 module.exports = router;
