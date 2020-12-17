9e98bf95f2038a818ec49fb2ddc546b8cf898a5d
taniamp98 <podoleanutanya@yahoo.com>
2020-12-17 21:18:24 +0200

    some bug routes

--- back-end/routes/bug.js
+++ back-end/routes/bug.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const bugController = require("../controllers/bug")
 
 router.post("/addbug", bugController.addBug);
+router.get("/seebugs", bugController.getBug);
+router.delete("/deleteOneBug/:id", bugController.deleteOneBug);
+router.get("/getonebug/:id", bugController.getOneBug);
 
 module.exports = router;
